f8824ef597ce37d47d8e06f37418bc639128bea4
test <test>
2025-11-17 08:12:35 +1100

    Initial Commit

---
+++ MealTracker/MealTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealTrackerApp.swift
+//  MealTracker
+//
+//  Created by Simon Lee on 17/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct MealTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
